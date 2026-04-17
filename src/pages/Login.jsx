8853a7d9d9e8af540952f354b5fd88b5fbeab9d0
Rahul Verma <167573663+RahulVerma2629@users.noreply.github.com>
2026-04-17 07:50:59 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/clerk-react";
 import "/styles/Auth.css";
 
 export default function Login() {
-  return (
+  return  (
     <div className="Login-auth-container">
       <div className="clerk-wrapper">
         <SignIn />
